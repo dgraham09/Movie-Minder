1e4a736f42b38fecdf0fa07c5d3d6253b743e519
david graham <davidcgraham09@gmail.com>
2023-09-27 13:09:39 +0200

    ModalAddedToSearchPage

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
 
-  static targets = ["input", "results"]
+  static targets = ["input", "results", "movie"]
 
   static values = {
     apiKey: String
@@ -20,13 +20,20 @@ export default class extends Controller {
       this.resultsTarget.innerHTML = ''
       data.results.forEach((result) => {
         const movieTag = `
-        <li>
+        <li class="movie-item">
             <img src=https://image.tmdb.org/t/p/w500${result.poster_path} style="width: 300px; height: 400px; border-radius: 15px; padding:0.2em; margin: 0.25rem">
+            <button class="info-button" data-action="click->search#toggleModal">Learn more</button>
         </li>
         `
         this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
       })
     })
 
+  }
+
+  toggleModal() {
+    console.log("This is connected")
+    const modal = document.getElementById("movieModal")
+    modal.style.display = "block"
   }
 }
